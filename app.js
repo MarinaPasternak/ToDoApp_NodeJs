053877f7b47b4464cbecd029426286f976264799
Marina_Pasternak <256pasternakmarina@gmail.com>
2024-04-17 21:13:49 +0300

    created view for app

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-app.use(express.static('./public'))
+app.use(express.static(__dirname + '/public'));
 
 toDoController(app);
 app.listen(3000);
